426b61f3185a8c7c7d7013415199eea6ada0f5ce
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-10-20 17:26:09 +1100

    Card animates on scroll

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import { gsap } from 'gsap';
 
 @Component({
@@ -6,19 +6,28 @@ import { gsap } from 'gsap';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
 
   @ViewChild('logo', {static: true}) logo: ElementRef;
-
+  @ViewChildren('navlist') menu: QueryList<ElementRef>;
+  menuItems: string[] = ['Projects', 'Contact', 'Resume'];
+  menuItemRefs: ElementRef[] = [];
   constructor() { }
 
   ngOnInit(): void {
-    gsap.from('.nav-list li', {
+  }
+
+  ngAfterViewInit(): void {
+    this.menuItemRefs = this.menu.map((elementRef) => elementRef.nativeElement);
+    this.navbarAnimation();
+  }
+
+  navbarAnimation() {
+    gsap.from(this.menuItemRefs, {
       duration: 0.5,
       translateY: -80,
       stagger: 0.2,
       ease: 'power3'
     });
   }
-
 }
--- src/app/components/projects/project/project.component.ts
+++ src/app/components/projects/project/project.component.ts
@@ -1,7 +1,8 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {gsap} from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import {Project} from '../Projects';
+import {element} from 'protractor';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,32 +11,73 @@ gsap.registerPlugin(ScrollTrigger);
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit, AfterViewInit {
   @Input() project: Project;
   @Input() isLeft: boolean;
 
   @ViewChild('card', { static: true }) private card: ElementRef;
   @ViewChild('details', { static: true }) private details: ElementRef;
-  tl = gsap.timeline();
+  @ViewChild('builtWith', { static: true }) private builtWith: ElementRef;
+  @ViewChildren('techList') private techList: QueryList<ElementRef>;
+  @ViewChild('buttons') private buttons: ElementRef;
+  tl: any;
   cardTl = gsap.timeline();
-  detailsTl = gsap.timeline();
 
   constructor() {}
 
+  ngAfterViewInit(): void {
+     this.slideCardUp();
+  }
+
   ngOnInit(): void {
-    // this.tl = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: this.details.nativeElement,
-    //     start: 'top top'
-    //   }
-    // });
-    // this.setUpTimeline();
-    gsap.from(this.details.nativeElement, {
-      scrollTrigger: this.details.nativeElement,
-      duration: 2,
-      translateX: this.isLeft ? 100 : -100,
+    // Setup Scrolltrigger and timeline defaults
+    this.tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.card.nativeElement,
+        start: 'top 80%'
+      },
+      defaults: {
+        duration: 0.8,
+        autoAlpha: 0,
+        ease: 'power1.inOut',
+      },
     });
+
+    // gsap.from(this.techList.nativeElement.childNodes, {
+    //   translateY: 20,
+    //   duration: 0.5,
+    //   stagger: 0.2,
+    //   delay: 2,
+    // });
+
     // TODO: Make description retractable for mobile
+  }
+
+  getChildrenRefs(queryList: QueryList<ElementRef>) {
+    return queryList.map((elementRef) => elementRef.nativeElement);
+  }
+
+
+  slideCardUp() {
+    this.tl
+      .from(this.card.nativeElement, {
+        translateY: 50,
+      })
+      .from(this.builtWith.nativeElement, {
+        translateY: 20,
+        duration: 0.3,
+      }, 'labels')
+      .from(this.getChildrenRefs(this.techList), {
+        translateY: 20,
+        duration: 0.5,
+        stagger: 0.2,
+      })
+      .from(Array.from(this.buttons.nativeElement.childNodes), {
+        translateY: 20,
+        duration: 0.5,
+        stagger: 0.2
+      })
+    ;
   }
 
 
@@ -50,14 +92,4 @@ export class ProjectComponent implements OnInit {
     this.cardTl.pause();
     reverse ? this.cardTl.reverse() : this.cardTl.play();
   }
-
-  // setUpTimeline() {
-  //   this.tl.from(this.details.nativeElement, {
-  //     duration: 1,
-  //     translateX: 100,
-  //     // opacity: 0
-  //   });
-  // }
-
-
 }
