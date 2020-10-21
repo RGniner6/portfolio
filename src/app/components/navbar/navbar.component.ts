95a28436ed7174649206610798526d544102ed8b
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-10-21 12:43:00 +1100

    Navbar has toggle animation on desktop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { gsap } from 'gsap';
 import {NavigationService} from '../../services/navigation.service';
+import {icon} from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,19 +11,58 @@ import {NavigationService} from '../../services/navigation.service';
 export class NavbarComponent implements OnInit {
 
   @ViewChild('navButton', {static: true}) navButton: ElementRef;
+  @ViewChild('topLine', {static: true}) topLine: ElementRef;
+  @ViewChild('bottomLine', {static: true}) bottomLine: ElementRef;
+  @ViewChild('navBox', {static: true}) navBox: ElementRef;
 
+  tl = gsap.timeline({
+    defaults: {
+      duration: 0.2,
+      ease: 'power1.inOut'
+    },
+    paused: true,
+    reversed: true
+  });
   constructor(private navigationService: NavigationService) { }
 
   ngOnInit(): void {
-    gsap.from('.nav-list li', {
-      duration: 0.5,
-      translateY: -80,
-      stagger: 0.2,
-      ease: 'power3'
-    });
+    // gsap.from('.nav-list li', {
+    //   duration: 0.5,
+    //   translateY: -80,
+    //   stagger: 0.2,
+    //   ease: 'power3'
+    // });
+    this.menuIconAnimation();
+    this.navBarAnimation();
+  }
+
+  menuIconAnimation() {
+    this.tl
+      .to(this.topLine.nativeElement, {
+        rotate: 45,
+        top: '50%',
+        backgroundColor: 'white'
+      }, 'toggle-menu')
+      .to(this.bottomLine.nativeElement, {
+        rotate: -45,
+        top: '50%',
+        backgroundColor: 'white'
+      }, 'toggle-menu');
+  }
+
+  navBarAnimation() {
+    this.tl
+      .to(this.navBox.nativeElement, {
+        right: 0
+      }, 'toggle-menu');
   }
 
   navigateTo(name: string) {
+    this.toggleMenu();
     this.navigationService.scrollto(name);
+  }
+
+  toggleMenu() {
+    this.tl.reversed() ? this.tl.play() : this.tl.reverse();
   }
 }
