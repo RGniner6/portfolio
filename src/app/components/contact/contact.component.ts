a3dd92907be3e8f6b8618f73ad432e68fe1ab19d
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-10-22 03:10:22 +1100

    Added animations to contact section

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -14,9 +14,15 @@ export class ContactComponent implements OnInit {
   @ViewChild('contactRef', {static: true}) contactElement: ElementRef;
   @ViewChild('sectionHeader', {static: true}) sectionHeader: ElementRef;
   @ViewChild('sectionDescription', {static: true}) sectionDesc: ElementRef;
+  @ViewChild('infoPanel', {static: true}) infoPanel: ElementRef;
+  @ViewChild('info', {static: true}) info: ElementRef;
+  @ViewChild('card', {static: true}) card: ElementRef;
+  @ViewChild('form', {static: true}) formRef: ElementRef;
+
   form: FormGroup;
   tl: any;
-  sectionTl: any;
+  cardTl: any;
+  infoTl: any;
   constructor(private navigationService: NavigationService,
               private http: HttpClient,
               private fb: FormBuilder) {
@@ -29,30 +35,45 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
     this.navigationService.addLink('contact', this.contactElement);
+
     this.tl = gsap.timeline({
       scrollTrigger: {
         trigger: this.sectionHeader.nativeElement,
         start: 'top 80%'
       },
       defaults: {
-        duration: 0.8,
+        duration: 0.3,
         autoAlpha: 0,
         ease: 'power1.inOut',
       },
     });
 
-    this.sectionTl = gsap.timeline({
+    this.cardTl = gsap.timeline({
       scrollTrigger: {
-        trigger: this.sectionHeader.nativeElement,
+        trigger: this.card.nativeElement,
         start: 'top 80%'
       },
       defaults: {
-        duration: 0.8,
+        duration: 0.3,
+        autoAlpha: 0,
+        ease: 'power1.inOut',
+      },
+    });
+
+    this.infoTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: this.infoPanel.nativeElement,
+        start: 'top 80%'
+      },
+      defaults: {
+        duration: 0.3,
         autoAlpha: 0,
         ease: 'power1.inOut',
       },
     });
     this.sectionHeaderAnimation();
+    this.setCardAnimation();
+    this.setInfoAnimation();
   }
 
   onSubmit() {
@@ -74,6 +95,30 @@ export class ContactComponent implements OnInit {
     this.tl
       .from(this.sectionHeader.nativeElement, {duration: 1, translateX: -160, opacity: 0, ease: 'power1.inOut'}, 'intro-text')
       .from(this.sectionDesc.nativeElement, {duration: 1.5, translateX: 130, scale: 1.2, opacity: 0,  }, 'intro-text');
+  }
+
+  setCardAnimation() {
+    this.cardTl
+      .from(this.card.nativeElement, {
+        duration: 0.8,
+        translateY: 50,
+      })
+      .from(this.formRef.nativeElement.childNodes, {
+        translateY: 20,
+        stagger: 0.2
+      });
+  }
+
+  setInfoAnimation() {
+    this.infoTl
+      .from(this.infoPanel.nativeElement, {
+        duration: 0.8,
+        translateY: 50,
+      })
+      .from(this.info.nativeElement.childNodes, {
+        translateY: 20,
+        stagger: 0.2
+      });
   }
 
   @HostListener('window:resize', ['$event'])
